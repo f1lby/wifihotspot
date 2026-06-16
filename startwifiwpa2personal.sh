615de9f5fb323430bd4f7c9ebcb4218383d104ab
F1lby <46318356+f1lby@users.noreply.github.com>
2026-06-16 23:39:24 +0100

    Update startwifiwpa2personal.sh

--- startwifiwpa2personal.sh
+++ startwifiwpa2personal.sh
@@ -6,5 +6,5 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo iptables --table nat --append POSTROUTING --out-interface eth0 -j MASQU>
 sudo iptables --append FORWARD --in-interface wlan0 -j ACCEPT
 sudo iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISH>
-sudo systemctl restart dnsmasq
+# sudo systemctl restart dnsmasq
 # sudo hostapd /etc/hostapd/hostapdwpa2personal.conf
